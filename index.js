6ec171196758337d0498e83ce424b4b65ff885a6
sairamsamala <sairamsamala14@gmail.com>
2026-05-30 13:34:07 +0530

    Feat: CI Pipeline

---
+++ index.js
@@ -0,0 +1,2 @@
+console.log("Hello DevOps");
+console.log("iam learning CI CD pipelines");
---
+++ test.js
@@ -0,0 +1,5 @@
+console.log("Statring test.....");
+setTimeout(() => {
+  console.log("Waiting for 3 sec...");
+}, 3000);
+console.log("test Completed....");
